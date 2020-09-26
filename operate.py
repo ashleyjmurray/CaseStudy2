157b69995216ff29893ea815d98eaf0087f687f7
Ashley Murray <42747966+ashleyjmurray@users.noreply.github.com>
2020-09-26 00:39:03 -0400

    Update operate.py

--- operate.py
+++ operate.py
@@ -152,45 +152,215 @@ for i in labels:
     for t in temp2:
         windows.append(t)
 
-y = []
+y = pd.DataFrame(columns = ['ECG_Rate_Mean', 'HRV_RMSSD', 'HRV_MeanNN', 'HRV_SDNN', 'HRV_SDSD','HRV_CVNN', 'HRV_CVSD', 'HRV_MedianNN', 'HRV_MadNN', 'HRV_MCVNN', 'HRV_IQRNN', 'HRV_pNN50', 'HRV_pNN20', 'HRV_TINN', 'HRV_HTI', 'HRV_ULF',
+'HRV_VLF', 'HRV_LF', 'HRV_HF', 'HRV_VHF', 'HRV_LFHF', 'HRV_LFn',
+'HRV_HFn', 'HRV_LnHF', 'HRV_SD1', 'HRV_SD2', 'HRV_SD1SD2', 'HRV_S',
+'HRV_CSI', 'HRV_CVI', 'HRV_CSI_Modified', 'HRV_PIP', 'HRV_IALS',
+'HRV_PSS', 'HRV_PAS', 'HRV_GI', 'HRV_SI', 'HRV_AI', 'HRV_PI', 'HRV_C1d',
+'HRV_C1a', 'HRV_SD1d', 'HRV_SD1a', 'HRV_C2d', 'HRV_C2a', 'HRV_SD2d',
+'HRV_SD2a', 'HRV_Cd', 'HRV_Ca', 'HRV_SDNNd', 'HRV_SDNNa', 'HRV_ApEn',
+'HRV_SampEn','eda_mean_chest',
+'eda_std_chest',
+'eda_min_chest',
+'eda_max_chest',
+'eda_slope_chest',
+'eda_range_chest',
+'eda_mean_scl_chest',
+'eda_std_scl_chest',
+'eda_std_scr_chest',
+'eda_scl_corr_chest',
+'eda_num_scr_seg_chest',
+'eda_sum_startle_mag_chest',
+'eda_sum_response_time_chest',
+'eda_sum_response_areas_chest',
+'eda_mean_wr',
+'eda_std_wr',
+'eda_min_wr',
+'eda_max_wr',
+'eda_slope_wr',
+'eda_range_wr',
+'eda_mean_scl_wr',
+'eda_std_scl_wr',
+'eda_std_scr_wr',
+'eda_scl_corr_wr',
+'eda_num_scr_seg_wr',
+'eda_sum_startle_mag_wr',
+'eda_sum_response_time_wr',
+'eda_sum_response_areas_wr',
+'resp_rate',
+  'mean_inhale_duration',
+  'std_inhale_duration',
+  'mean_exhale_duration',
+  'std_exhale_duration',
+  'ie_ratio',
+  'resp_stretch',
+  'temp_wr_mean',
+  'temp_wr_standard_deviation',
+  'temp_wr_tenth_quantile',
+  'temp_wr_nintieth_quantile',
+  'temp_wr_range',
+  'temp_chest_mean',
+  'temp_chest_standard_deviation',
+  'temp_chest_tenth_quantile',
+  'temp_chest_nintieth_quantile',
+  'temp_chest_range', 'subject', 'label'])
+
+
+
+
 for i in range(len(windows)):
-    #df = pd.DataFrame()
+    
+    subject = windows[i]['subject'].head(1).values[0]
+    label = windows[i]['label'].head(1).values[0]
     ecg_data = process_ecg(windows[i])
-    ecg_data['subject'] = windows[i]['subject'].head(1).values[0]
-    ecg_data['window_trial'] = i
-    ecg_data['label'] = windows[i]['label'].head(1).values[0]
-    y.append(ecg_data)
+    #ecg_data['subject'] = windows[i]['subject'].head(1).values[0]
+    #ecg_data['window_trial'] = i
+    #ecg_data['label'] = windows[i]['label'].head(1).values[0]
+    
+    ECG_Rate_Mean = ecg_data.ECG_Rate_Mean.values[0]
+    HRV_RMSSD = ecg_data.HRV_RMSSD.values[0]
+    HRV_MeanNN= ecg_data.HRV_MeanNN.values[0]
+    HRV_SDNN= ecg_data.HRV_SDNN.values[0]
+    HRV_SDNN= ecg_data.HRV_SDNN.values[0]
+    HRV_CVNN= ecg_data.HRV_CVNN.values[0]
+    HRV_CVSD= ecg_data.HRV_CVSD.values[0]
+    HRV_MedianNN= ecg_data.HRV_MedianNN.values[0]
+    HRV_MadNN= ecg_data.HRV_MadNN.values[0]
+    HRV_MCVNN= ecg_data.HRV_MCVNN.values[0]
+    HRV_IQRNN= ecg_data.HRV_IQRNN.values[0]
+    HRV_pNN50= ecg_data.HRV_pNN50.values[0]
+    HRV_pNN20= ecg_data.HRV_pNN20.values[0]
+    HRV_TINN= ecg_data.HRV_TINN.values[0]
+    HRV_HTI= ecg_data.HRV_HTI.values[0]
+    HRV_ULF= ecg_data.HRV_ULF.values[0]
+    HRV_VLF= ecg_data.HRV_VLF.values[0]
+    HRV_LF= ecg_data.HRV_LF.values[0]
+    HRV_HF= ecg_data.HRV_HF.values[0]
+    HRV_VHF= ecg_data.HRV_VHF.values[0]
+    HRV_LFHF= ecg_data.HRV_LFHF.values[0]
+    HRV_LFn= ecg_data.HRV_LFn.values[0]
+    HRV_HFn= ecg_data.HRV_HFn.values[0]
+    HRV_LnHF= ecg_data.HRV_LnHF.values[0]
+    HRV_SD1= ecg_data.HRV_SD1.values[0]
+    HRV_SD2= ecg_data.HRV_SD2.values[0]
+    HRV_SD1SD2= ecg_data.HRV_SD1SD2.values[0]
+    HRV_S= ecg_data.HRV_S.values[0]
+    HRV_CSI= ecg_data.HRV_CSI.values[0]
+    HRV_CVI= ecg_data.HRV_CVI.values[0]
+    HRV_CSI_Modified= ecg_data.HRV_CSI_Modified.values[0]
+    HRV_PIP= ecg_data.HRV_PIP.values[0]
+    HRV_IALS= ecg_data.HRV_IALS.values[0]
+    HRV_PSS= ecg_data.HRV_PSS.values[0]
+    HRV_PAS= ecg_data.HRV_PAS.values[0]
+    HRV_GI= ecg_data.HRV_GI.values[0]
+    HRV_SI= ecg_data.HRV_SI.values[0]
+    HRV_AI= ecg_data.HRV_AI.values[0]
+    HRV_PI= ecg_data.HRV_PI.values[0]
+    HRV_C1d= ecg_data.HRV_C1d.values[0]
+    HRV_C1a= ecg_data.HRV_C1a.values[0]
+    HRV_SD1d= ecg_data.HRV_SD1d.values[0]
+    HRV_SD1a= ecg_data.HRV_SD1a.values[0]
+    HRV_C2d= ecg_data.HRV_C2d.values[0]
+    HRV_C2a= ecg_data.HRV_C2a.values[0]
+    HRV_SD2d= ecg_data.HRV_SD2d.values[0]
+    HRV_SD2a= ecg_data.HRV_SD2a.values[0]
+    HRV_Cd= ecg_data.HRV_Cd.values[0]
+    HRV_Ca= ecg_data.HRV_Ca.values[0]
+    HRV_SDNNd= ecg_data.HRV_SDNNd.values[0]
+    HRV_SDNNa= ecg_data.HRV_SDNNa.values[0]
+    HRV_ApEn= ecg_data.HRV_ApEn.values[0]
+    HRV_SampEn= ecg_data.HRV_SampEn.values[0]
+    HRV_SDSD = ecg_data.HRV_SDSD.values[0]
+    
+    #EDA Features ~~
     
     eda_data = get_eda_features(windows[i]['EDA_x'])
-    eda_data['subject'] = windows[i]['subject'].head(1).values[0]
-    eda_data["window_trial"] = i
-    eda_data['label'] = windows[i]['label'].head(1).values[0]
-    eda_data['wrist_or_chest'] = 'chest'
-    y.append(eda_data)
+    #eda_data['subject'] = windows[i]['subject'].head(1).values[0]
+    #eda_data["window_trial"] = i
+    #eda_data['label'] = windows[i]['label'].head(1).values[0]
+    #eda_data['wrist_or_chest'] = 'chest'
+    
+    eda_mean_chest = eda_data.mean_eda.values[0]
+    eda_std_chest = eda_data.std_eda.values[0]
+    eda_min_chest = eda_data.min_eda.values[0]
+    eda_max_chest = eda_data.max_eda.values[0]
+    eda_slope_chest = eda_data.slope_eda.values[0]
+    eda_range_chest = eda_data.range_eda.values[0]
+    eda_mean_scl_chest = eda_data.mean_scl.values[0]
+    eda_std_scl_chest = eda_data.std_scl.values[0]
+    eda_std_scr_chest = eda_data.std_scr.values[0]
+    eda_scl_corr_chest = eda_data.scl_corr.values[0]
+    eda_num_scr_seg_chest = eda_data.num_scr_seg.values[0]
+    eda_sum_startle_mag_chest = eda_data.sum_startle_mag.values[0]
+    eda_sum_response_time_chest = eda_data.sum_response_time.values[0]
+    eda_sum_response_areas_chest = eda_data.sum_response_time.values[0]
+    
+    
+    #y.append(eda_data)
     
     eda_data_wr = get_eda_features(windows[i]['EDA_y'], sample_rate=4)
-    eda_data_wr['subject'] = windows[i]['subject'].head(1).values[0]
-    eda_data_wr["window_trial"] = i
-    eda_data_wr['label'] = windows[i]['label'].head(1).values[0]
-    eda_data_wr['wrist_or_chest'] = 'wrist'
-    y.append(eda_data_wr)
+    #eda_data_wr['subject'] = windows[i]['subject'].head(1).values[0]
+    #eda_data_wr["window_trial"] = i
+    #eda_data_wr['label'] = windows[i]['label'].head(1).values[0]
+    #eda_data_wr['wrist_or_chest'] = 'wrist'
+    #y.append(eda_data_wr)
+    eda_mean_wr = eda_data_wr.mean_eda.values[0]
+    eda_std_wr = eda_data_wr.std_eda.values[0]
+    eda_min_wr = eda_data_wr.min_eda.values[0]
+    eda_max_wr = eda_data_wr.max_eda.values[0]
+    eda_slope_wr = eda_data_wr.slope_eda.values[0]
+    eda_range_wr = eda_data_wr.range_eda.values[0]
+    eda_mean_scl_wr = eda_data_wr.mean_scl.values[0]
+    eda_std_scl_wr = eda_data_wr.std_scl.values[0]
+    eda_std_scr_wr = eda_data_wr.std_scr.values[0]
+    eda_scl_corr_wr = eda_data_wr.scl_corr.values[0]
+    eda_num_scr_seg_wr = eda_data_wr.num_scr_seg.values[0]
+    eda_sum_startle_mag_wr = eda_data_wr.sum_startle_mag.values[0]
+    eda_sum_response_time_wr = eda_data_wr.sum_response_time.values[0]
+    eda_sum_response_areas_wr = eda_data_wr.sum_response_time.values[0]
     
+    
+    # Resp Features ~~
     resp_data = resp_features(windows[i]['Resp'])
     resp_data['subject'] = windows[i]['subject'].head(1).values[0]
     resp_data['window_trial'] = i
-    y.append(resp_data)
     
+    resp_rate = resp_data.resp_rate.values[0]
+    mean_inhale_duration = resp_data.mean_inhale_duration.values[0]
+    std_inhale_duration = resp_data.std_inhale_duration.values[0]
+    mean_exhale_duration = resp_data.mean_exhale_duration.values[0]
+    std_exhale_duration = resp_data.std_exhale_duration.values[0]
+    ie_ratio = resp_data.ie_ratio.values[0]
+    resp_stretch = resp_data.resp_stretch.values[0]
+    
+    #y.append(resp_data)
+    
+    #Temp Features ~
     temp_features_wr = process_temp(windows[i], True)
-    temp_features_wr['subject'] = windows[i]['subject'].head(1).values[0]
-    temp_features_wr['window_trial'] = i
-    temp_features_wr['wrist_or_chest'] = 'wrist'
-    y.append(temp_features_wr)
+    #temp_features_wr['subject'] = windows[i]['subject'].head(1).values[0]
+    #temp_features_wr['window_trial'] = i
+    #temp_features_wr['wrist_or_chest'] = 'wrist'
+    
+    temp_wr_mean = temp_features_wr.temp_mean.values[0]
+    temp_wr_standard_deviation = temp_features_wr.temp_standard_deviation.values[0]
+    temp_wr_tenth_quantile = temp_features_wr.temp_tenth_quantile.values[0]
+    temp_wr_nintieth_quantile = temp_features_wr.temp_nintieth_quantile.values[0]
+    temp_wr_range = temp_features_wr.temp_range.values[0]
     
     temp_features = process_temp(windows[i], False)
-    temp_features['subject'] = windows[i]['subject'].head(1).values[0]
-    temp_features['window_trial'] = i
-    temp_features['wrist_or_chest'] = 'chest'
-    y.append(temp_features)
+    #temp_features['subject'] = windows[i]['subject'].head(1).values[0]
+    #temp_features['window_trial'] = i
+    #temp_features['wrist_or_chest'] = 'chest'
+    #y.append(temp_features)
     
-final_df = pd.concat(y)
-final_df.to_csv("final.csv")
+    temp_chest_mean = temp_features.temp_mean.values[0]
+    temp_chest_standard_deviation = temp_features.temp_standard_deviation.values[0]
+    temp_chest_tenth_quantile = temp_features.temp_tenth_quantile.values[0]
+    temp_chest_nintieth_quantile = temp_features.temp_nintieth_quantile.values[0]
+    temp_chest_range = temp_features.temp_range.values[0]
+    
+    
+    y.loc[len(y)] = [ECG_Rate_Mean, HRV_RMSSD, HRV_MeanNN, HRV_SDNN, HRV_SDSD,HRV_CVNN, HRV_CVSD, HRV_MedianNN, HRV_MadNN, HRV_MCVNN, HRV_IQRNN, HRV_pNN50, HRV_pNN20, HRV_TINN, HRV_HTI, HRV_ULF,HRV_VLF, HRV_LF, HRV_HF, HRV_VHF, HRV_LFHF, HRV_LFn,HRV_HFn, HRV_LnHF, HRV_SD1, HRV_SD2, HRV_SD1SD2, HRV_S,HRV_CSI, HRV_CVI,HRV_CSI_Modified, HRV_PIP, HRV_IALS,HRV_PSS, HRV_PAS, HRV_GI, HRV_SI, HRV_AI, HRV_PI, HRV_C1d,HRV_C1a, HRV_SD1d, HRV_SD1a, HRV_C2d, HRV_C2a, HRV_SD2d,HRV_SD2a, HRV_Cd, HRV_Ca, HRV_SDNNd, HRV_SDNNa, HRV_ApEn,HRV_SampEn,eda_mean_chest,eda_std_chest,eda_min_chest,eda_max_chest,eda_slope_chest,eda_range_chest,eda_mean_scl_chest, eda_std_scl_chest,eda_std_scr_chest,eda_scl_corr_chest,eda_num_scr_seg_chest,eda_sum_startle_mag_chest,eda_sum_response_time_chest,eda_sum_response_areas_chest,eda_mean_wr,eda_std_wr,eda_min_wr, eda_max_wr,eda_slope_wr,eda_range_wr,eda_mean_scl_wr,eda_std_scl_wr,eda_std_scr_wr,eda_scl_corr_wr,eda_num_scr_seg_wr,eda_sum_startle_mag_wr,eda_sum_response_time_wr,eda_sum_response_areas_wr, resp_rate,mean_inhale_duration,std_inhale_duration,mean_exhale_duration,std_exhale_duration,ie_ratio,resp_stretch,temp_wr_mean,temp_wr_standard_deviation,temp_wr_tenth_quantile,temp_wr_nintieth_quantile, temp_wr_range,temp_chest_mean,temp_chest_standard_deviation,temp_chest_tenth_quantile,temp_chest_nintieth_quantile,temp_chest_range, subject, label]
+    
+
+y.to_csv("final.csv")
